94e161e071b3517ad7b278a8d551a9d99d92ad13
Nabil Boag <nabilgod@gmail.com>
2018-08-11 16:24:00 +0000

    Adding app sync

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+export default {
+    "graphqlEndpoint": "https://ygu752k5xnhibjyb24skjefqei.appsync-api.eu-west-1.amazonaws.com/graphql",
+    "region": "eu-west-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-p4uqzrvf7zg6diwlbnxltnzhdy"
+}
